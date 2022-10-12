b2896bf61688fa4c4f3b8c0ce244bd0dcff02de5
swhoxf <108375735+swhoxf@users.noreply.github.com>
2022-10-12 19:57:24 -0400

    restructured character image window

--- my-app/src/CharacterWindow.js
+++ my-app/src/CharacterWindow.js
@@ -3,7 +3,10 @@ import './CharacterWindow.css'
 function CharacterWindow() {
   return (
     <div className='characterWindowContainer'>
-      <div className="dummyCharacterImage">character image</div>
+      <div className='characterImageContainer'>
+        <div className="dummyCharacterImage">character image</div>
+      </div>
+      
       <div className="informationBox">
         <h2 className='characterName'>Character name</h2>
         <h3 className='pronouns'>Pronouns</h3>
